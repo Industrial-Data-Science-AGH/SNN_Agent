0fdc3aa3f0e9b6b569f79bf9d52245d3c3152ac1
kuleczkomen <kuleczkomen@gmail.com>
2026-05-16 22:36:36 +0200

    pierwsze udane testy z mikrofonem

--- encoder/snn_encoder_params_hpf/snn_encoder_params_hpf.ino
+++ encoder/snn_encoder_params_hpf/snn_encoder_params_hpf.ino
@@ -23,14 +23,13 @@
 const uint8_t SPIKE_PINS[3] = {PIN_SPIKE_PEAK, PIN_SPIKE_MEAN, PIN_SPIKE_STD};
 
 // ---- PARAMETRY WSPÓLNE ----
-#define DC_OFFSET      610  // Wartość spoczynkowa dla ciszy; ok 940 dla 5V i 610 dla 3.3V
 #define FRAME_WINDOW_MS 20  // ms - rozmiar ramki czasowej do analizy energii
 
 // Znormalizowane maksima dla poszczególnych cech (do kalibracji)
 // Pozwalają mapować wartości z ADC na zakres [0.0 - 1.0]
-#define MAX_PEAK_VAL   512.0f 
-#define MAX_MEAN_VAL   250.0f
-#define MAX_STD_VAL    300.0f
+#define MAX_PEAK_VAL   200.0f 
+#define MAX_MEAN_VAL   40.0f
+#define MAX_STD_VAL    30.0f
 const float MAX_VALS[3] = {MAX_PEAK_VAL, MAX_MEAN_VAL, MAX_STD_VAL};
 
 // ---- PARAMETRY RATE CODING ----
@@ -54,6 +53,11 @@ static uint32_t spikeCount[3]    = {0, 0, 0};
 
 float channelValues[3] = {0.0f, 0.0f, 0.0f}; // [Peak, Mean, Std]
 float smoothedVals[3]  = {0.0f, 0.0f, 0.0f}; // Filtracja Low-pass dla RC
+
+// ---- ZMIENNE FILTRA HPF ----
+float hp_filtered = 0.0f;
+float prev_raw = 610.0f; // spodziewana wartość spoczynkowa: 610 dla 3.3V, 940 dla 5V
+#define ALPHA 0.99f // współczynnik odcięcia DC (ok 0.95-0.99)
 
 
 // ============================================================
@@ -85,33 +89,37 @@ void setup() {
 // ============================================================
 //  ODCZYT STATYSTYK MIKROFONU (Energy Extraction)
 //  Wylicza parametry na liczbach całkowitych dla oszczędności CPU.
+// zastosowano filtr HPF
 // ============================================================
 
 void extractFrameFeatures(uint16_t windowMs) {
   uint32_t start = millis();
-  int16_t maxAc = 0;
-  uint32_t sumAc = 0;
-  uint32_t sumSq = 0;
+  float maxAc = 0;
+  float sumAc = 0;
+  float sumSq = 0;
   uint16_t count = 0;
 
   while (millis() - start < windowMs) {
-    int16_t val = abs(analogRead(PIN_MIC) - DC_OFFSET);
-    maxAc = max(val, maxAc);
+    float raw = (float) analogRead(PIN_MIC);
 
+    hp_filtered = ALPHA * (hp_filtered + raw - prev_raw);
+    prev_raw = raw;
+
+    float val = abs(hp_filtered);
+    
+    maxAc = max(maxAc, val);
     sumAc += val;
-    sumSq += (int32_t) val * val;
+    sumSq += val * val;
     count++;
   }
 
   // Zabezpieczenie przed dzieleniem przez zero (w razie zacięcia zegara)
-  if(count == 0) {
-    count = 1;
-  }
+  if(count == 0) count = 1;
 
   // Obliczenia końcowe (float używany tylko tu, raz na okno)
-  channelValues[0] = (float) maxAc;         // PEAK
-  channelValues[1] = (float) sumAc / count; // MEAN
-  float variance = (float) sumSq / count;
+  channelValues[0] = maxAc;         // PEAK
+  channelValues[1] = sumAc / (float) count; // MEAN
+  float variance = sumSq / (float) count;
   channelValues[2] = sqrt(variance);        // STD
 }
 
@@ -119,6 +127,7 @@ void extractFrameFeatures(uint16_t windowMs) {
 //  GENERUJ SPIKE NA KONKRETNYM KANALE
 // ============================================================
 void fireSpike(uint8_t channel) {
+  Serial.print("SPIKE "); Serial.println(channel);
   digitalWrite(SPIKE_PINS[channel], SPIKE_VOLTAGE);
   digitalWrite(PIN_DEBUG_LED, HIGH);
   delayMicroseconds(SPIKE_WIDTH_US);
@@ -143,7 +152,7 @@ void loopRateCoding() {
       smoothedVals[i] = 0.3f * channelValues[i] + 0.7f * smoothedVals[i];
 
       // Normalizacja do zakresu [0.0 - 1.0]
-      float normalised = max(smoothedVals[i] / MAX_VALS[i], 1.0f);
+      float normalised = min(smoothedVals[i] / MAX_VALS[i], 1.0f);
       if(normalised < RC_NOISE_FLOOR) {
         currISI_us[i] = 0; // cisza
       } else {
@@ -163,11 +172,16 @@ void loopRateCoding() {
       }
     }
   }
-  Serial.print("A0 = "); Serial.print(analogRead(0));
+
+  //  debug
+  if(smoothedVals[2] > 1.0) {
+    Serial.print("A0 = "); Serial.print(analogRead(0));
   Serial.print(" Peak = "); Serial.print(smoothedVals[0]);
   Serial.print(" Mean = "); Serial.print(smoothedVals[1]);
   Serial.print(" Stdev = "); Serial.print(smoothedVals[2]);
   Serial.println();
+  }
+  
 
 
 }
@@ -190,7 +204,7 @@ void loopTTFS() {
 
     for(int i=0; i<3; i++) {
       ttfsSpiked[i] = false;
-      float normalised = max(channelValues[i] / MAX_VALS[i], 1.0f);
+      float normalised = min(channelValues[i] / MAX_VALS[i], 1.0f);
 
       if(normalised < TTFS_THRESHOLD) {
         currISI_us[i] = 0;
